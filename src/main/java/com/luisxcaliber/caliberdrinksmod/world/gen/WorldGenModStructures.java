41466fdebec1fc57bf48f268fbe9028a5a57ef9b
Luisxcaliber <68454795+Luisxcaliber@users.noreply.github.com>
2021-07-09 22:41:24 -0400

    WorldGen Update

--- src/main/java/com/luisxcaliber/caliberdrinksmod/world/gen/WorldGenModStructures.java
+++ src/main/java/com/luisxcaliber/caliberdrinksmod/world/gen/WorldGenModStructures.java
@@ -12,7 +12,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeForest;
+import net.minecraft.world.biome.BiomeJungle;
 import net.minecraft.world.biome.BiomePlains;
+import net.minecraft.world.biome.BiomeSavanna;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -33,6 +35,54 @@ public class WorldGenModStructures implements IWorldGenerator
 	public final WorldGenStructure LEMONBUSH2 = new WorldGenStructure("lemonbush2");
 	public final WorldGenStructure LEMONBUSH3 = new WorldGenStructure("lemonbush3");
 	
+	public final WorldGenStructure GRAPEFRUITBUSH1 = new WorldGenStructure("grapefruitbush1");
+	public final WorldGenStructure GRAPEFRUITBUSH2 = new WorldGenStructure("grapefruitbush2");
+	public final WorldGenStructure GRAPEFRUITBUSH3 = new WorldGenStructure("grapefruitbush3");
+	
+	public final WorldGenStructure PINEAPPLEBUSH1 = new WorldGenStructure("pineapplebush1");
+	public final WorldGenStructure PINEAPPLEBUSH2 = new WorldGenStructure("pineapplebush2");
+	public final WorldGenStructure PINEAPPLEBUSH3 = new WorldGenStructure("pineapplebush3");
+	
+	public final WorldGenStructure STRAWBERRYBUSH1 = new WorldGenStructure("strawberrybush1");
+	public final WorldGenStructure STRAWBERRYBUSH2 = new WorldGenStructure("strawberrybush2");
+	public final WorldGenStructure STRAWBERRYBUSH3 = new WorldGenStructure("strawberrybush3");
+	
+	public final WorldGenStructure COFFEEBEANBUSH1 = new WorldGenStructure("coffeebeanbush1");
+	public final WorldGenStructure COFFEEBEANBUSH2 = new WorldGenStructure("coffeebeanbush2");
+	public final WorldGenStructure COFFEEBEANBUSH3 = new WorldGenStructure("coffeebeanbush3");
+	
+	public final WorldGenStructure COCONUTBUSH1 = new WorldGenStructure("coconutbush1");
+	public final WorldGenStructure COCONUTBUSH2 = new WorldGenStructure("coconutbush2");
+	public final WorldGenStructure COCONUTBUSH3 = new WorldGenStructure("coconutbush3");
+	
+	public final WorldGenStructure TEALEAFBUSH1 = new WorldGenStructure("tealeafbush1");
+	public final WorldGenStructure TEALEAFBUSH2 = new WorldGenStructure("tealeafbush2");
+	public final WorldGenStructure TEALEAFBUSH3 = new WorldGenStructure("tealeafbush3");
+	
+	public final WorldGenStructure WHITEGRAPEBUSH1 = new WorldGenStructure("whitegrapebush1");
+	public final WorldGenStructure WHITEGRAPEBUSH2 = new WorldGenStructure("whitegrapebush2");
+	public final WorldGenStructure WHITEGRAPEBUSH3 = new WorldGenStructure("whitegrapebush3");
+	
+	public final WorldGenStructure CRANBERRYBUSH1 = new WorldGenStructure("cranberrybush1");
+	public final WorldGenStructure CRANBERRYBUSH2 = new WorldGenStructure("cranberrybush2");
+	public final WorldGenStructure CRANBERRYBUSH3 = new WorldGenStructure("cranberrybush3");
+	
+	public final WorldGenStructure TOMATOBUSH1 = new WorldGenStructure("tomatobush1");
+	public final WorldGenStructure TOMATOBUSH2 = new WorldGenStructure("tomatobush2");
+	public final WorldGenStructure TOMATOBUSH3 = new WorldGenStructure("tomatobush3");
+	
+	public final WorldGenStructure POMEGRANATEBUSH1 = new WorldGenStructure("pomegranatebush1");
+	public final WorldGenStructure POMEGRANATEBUSH2 = new WorldGenStructure("pomegranatebush2");
+	public final WorldGenStructure POMEGRANATEBUSH3 = new WorldGenStructure("pomegranatebush3");
+	
+	public final WorldGenStructure PEACHBUSH1 = new WorldGenStructure("peachbush1");
+	public final WorldGenStructure PEACHBUSH2 = new WorldGenStructure("peachbush2");
+	public final WorldGenStructure PEACHBUSH3 = new WorldGenStructure("peachbush3");
+	
+	public final WorldGenStructure MANGOBUSH1 = new WorldGenStructure("mangobush1");
+	public final WorldGenStructure MANGOBUSH2 = new WorldGenStructure("mangobush2");
+	public final WorldGenStructure MANGOBUSH3 = new WorldGenStructure("mangobush3");
+	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) 
 	{
@@ -44,17 +94,65 @@ public class WorldGenModStructures implements IWorldGenerator
 			
 		case 0:
 			
-			generateStructure(GRAPEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomePlains.class, BiomeForest.class);
+			generateStructure(GRAPEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class, BiomePlains.class);
 			generateStructure(GRAPEBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
 			generateStructure(GRAPEBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
 			
-			generateStructure(ORANGEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomePlains.class, BiomeForest.class);
+			generateStructure(ORANGEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class, BiomePlains.class);
 			generateStructure(ORANGEBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
 			generateStructure(ORANGEBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
 			
-			generateStructure(LEMONBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomePlains.class, BiomeForest.class);
+			generateStructure(LEMONBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class, BiomePlains.class);
 			generateStructure(LEMONBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
 			generateStructure(LEMONBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(GRAPEFRUITBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			generateStructure(GRAPEFRUITBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			generateStructure(GRAPEFRUITBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			
+			generateStructure(PINEAPPLEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(PINEAPPLEBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(PINEAPPLEBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(STRAWBERRYBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class, BiomePlains.class);
+			generateStructure(STRAWBERRYBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(STRAWBERRYBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(COFFEEBEANBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(COFFEEBEANBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(COFFEEBEANBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			
+			generateStructure(COCONUTBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(COCONUTBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(COCONUTBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			
+			generateStructure(TEALEAFBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(TEALEAFBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(TEALEAFBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			
+			generateStructure(WHITEGRAPEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(WHITEGRAPEBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(WHITEGRAPEBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(CRANBERRYBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(CRANBERRYBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(CRANBERRYBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(TOMATOBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(TOMATOBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(TOMATOBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(POMEGRANATEBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			generateStructure(POMEGRANATEBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			generateStructure(POMEGRANATEBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeSavanna.class);
+			
+			generateStructure(PEACHBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class, BiomePlains.class);
+			generateStructure(PEACHBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			generateStructure(PEACHBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeForest.class);
+			
+			generateStructure(MANGOBUSH1, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(MANGOBUSH2, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
+			generateStructure(MANGOBUSH3, world, random, chunkX, chunkZ, 120, Blocks.GRASS, BiomeJungle.class);
 			
 			break;
 			
